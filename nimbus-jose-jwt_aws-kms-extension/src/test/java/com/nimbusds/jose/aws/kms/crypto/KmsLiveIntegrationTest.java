f73e8520ca770ecc381c2d9b022536c040b77b43
Joshua Harwood <joshua.harwood@tracsis.com>
2026-02-16 02:23:18 +0000

    Show debug logs in tests, manually test signing and verifying. Both work

--- nimbus-jose-jwt_aws-kms-extension/src/test/java/com/nimbusds/jose/aws/kms/crypto/KmsLiveIntegrationTest.java
+++ nimbus-jose-jwt_aws-kms-extension/src/test/java/com/nimbusds/jose/aws/kms/crypto/KmsLiveIntegrationTest.java
@@ -7,17 +7,18 @@ import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.kms.model.EncryptionAlgorithmSpec;
+import software.amazon.awssdk.services.kms.model.MessageType;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@Disabled
+@Disabled
 @Tag("LiveIntegration")
 public class KmsLiveIntegrationTest {
-    String keyId = "REPLACEME";
+    private String signingKeyId = "REPLACEME";
+    private String encryptionKeyId = "REPLACEME";
 
     @Test
-    void testDefaultEncrypterDecrypterWithLiveKms() throws Exception {
+    void testDefaultEncrypterDecrypterEncryptsSuccessfully() throws Exception {
         KmsClient kmsClient = KmsClient.builder().region(Region.EU_WEST_1).build();
         String payload = "Hello, live KMS!";
 
@@ -26,24 +27,38 @@ public class KmsLiveIntegrationTest {
 
         // 1. Encrypt
         JWEHeader header = new JWEHeader.Builder(symmetricDefaultAlgo, EncryptionMethod.A256GCM)
-                .keyID(keyId)
+                .keyID(encryptionKeyId)
                 .build();
         JWEObject jweObject = new JWEObject(header, new Payload(payload));
 
-        KmsDefaultEncrypter encrypter = new KmsDefaultEncrypter(kmsClient, keyId);
+        KmsDefaultEncrypter encrypter = new KmsDefaultEncrypter(kmsClient, encryptionKeyId);
         jweObject.encrypt(encrypter);
         String serialized = jweObject.serialize();
 
         // 2. Decrypt
         JWEObject parsedJwe = JWEObject.parse(serialized);
-        KmsDefaultDecrypter decrypter = new KmsDefaultDecrypter(kmsClient, keyId);
+        KmsDefaultDecrypter decrypter = new KmsDefaultDecrypter(kmsClient, encryptionKeyId);
         parsedJwe.decrypt(decrypter);
 
         assertThat(payload).isEqualTo(parsedJwe.getPayload().toString());
     }
 
     @Test
-    void testAsymmetricSignerWithLiveKms() throws Exception {
+    void testAsymmetricSignerSignsRawMessageSuccessfully() throws Exception {
+        KmsClient kmsClient = KmsClient.builder().region(Region.EU_WEST_1).build();
+        String payload = "Hello, live KMS! This is a longer message that we've signed.";
 
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS512).keyID(signingKeyId).build();
+        JWSObject jwsObject = new JWSObject(header, new Payload(payload));
+
+        KmsAsymmetricSigner signer = new KmsAsymmetricSigner(kmsClient, signingKeyId, MessageType.RAW);
+
+        jwsObject.sign(signer);
+
+        KmsAsymmetricVerifier verifier = new KmsAsymmetricVerifier(kmsClient, signingKeyId, MessageType.RAW);
+
+        boolean isValid = jwsObject.verify(verifier);
+
+        assertThat(isValid).isTrue();
     }
 }
